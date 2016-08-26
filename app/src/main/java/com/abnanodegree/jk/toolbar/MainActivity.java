64c8d8a59d33e7e90dffea7991032706842260e0
plato1 <blah@blah.com>
2016-08-25 23:00:56 -0500

    Cant get icon to appear on toolbar...but text for icon shows up in overflow

---
+++ app/src/main/java/com/abnanodegree/jk/toolbar/MainActivity.java
@@ -0,0 +1,26 @@
+package com.abnanodegree.jk.toolbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // setup toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_tools, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
